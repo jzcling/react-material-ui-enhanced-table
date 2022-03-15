cb490f1344626f9f58c80c20bc3cb3e8b3ace6bb
Jeremy <jlzc89@gmail.com>
2022-03-15 14:05:54 +0800

    export header and action interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import EnhancedTable, { EnhancedTableProps } from "./components/EnhancedTable";
+import { Action } from "./contracts/Action";
+import { Header } from "./contracts/Header";
 
 export default EnhancedTable;
 
-export { EnhancedTableProps };
+export { EnhancedTableProps, Action, Header };
